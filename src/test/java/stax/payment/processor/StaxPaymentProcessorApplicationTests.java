f828659b70cf2a7baac0201c40a04c62fbaa6774
Laila1985 <laila.bouhadou@gmail.com>
2026-09-23 13:21:31 +0300

    fix build

--- src/test/java/stax/payment/processor/StaxPaymentProcessorApplicationTests.java
+++ src/test/java/stax/payment/processor/StaxPaymentProcessorApplicationTests.java
@@ -1,11 +1,46 @@
 package stax.payment.processor;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 
 @SpringBootTest
-class StaxPaymentProcessorApplicationTests {
+@Testcontainers
+class PaymentXmlStreamProcessorApplicationTests {
 
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16")
+                    .withDatabaseName("payments")
+                    .withUsername("payment")
+                    .withPassword("payment");
 
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
 
+        registry.add(
+                "spring.datasource.url",
+                postgres::getJdbcUrl
+        );
+
+        registry.add(
+                "spring.datasource.username",
+                postgres::getUsername
+        );
+
+        registry.add(
+                "spring.datasource.password",
+                postgres::getPassword
+        );
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
